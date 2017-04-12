bb804542454fb4d7ce15e5383c160f31cfed9d19
absentje <absent37@list.ru>
2017-04-12 03:12:32 +0300

    Update dialog.h

--- 1/dialog.h
+++ 1/dialog.h
@@ -9,11 +9,6 @@
 class Dialog : public QWidget
 {
     Q_OBJECT
-
-    QLineEdit *m_name;
-    QPushButton *m_bln0k;
-    QPushButton *m_btnExit;
-
 public:
     Dialog(QWidget *parent = 0);
     ~Dialog();
