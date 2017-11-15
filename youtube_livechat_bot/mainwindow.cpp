0191cc663e52afd3de5504981d1b839f8f45293d
absentje <absent37@list.ru>
2017-11-15 13:32:07 +0300

    Update mainwindow.cpp

--- youtube_livechat_bot/mainwindow.cpp
+++ youtube_livechat_bot/mainwindow.cpp
@@ -1,53 +1,3 @@
-/****************************************************************************
-**
-** Copyright (C) 2016 The Qt Company Ltd.
-** Contact: https://www.qt.io/licensing/
-**
-** This file is part of the examples of the Qt Toolkit.
-**
-** $QT_BEGIN_LICENSE:BSD$
-** Commercial License Usage
-** Licensees holding valid commercial Qt licenses may use this file in
-** accordance with the commercial license agreement provided with the
-** Software or, alternatively, in accordance with the terms contained in
-** a written agreement between you and The Qt Company. For licensing terms
-** and conditions see https://www.qt.io/terms-conditions. For further
-** information use the contact form at https://www.qt.io/contact-us.
-**
-** BSD License Usage
-** Alternatively, you may use this file under the terms of the BSD license
-** as follows:
-**
-** "Redistribution and use in source and binary forms, with or without
-** modification, are permitted provided that the following conditions are
-** met:
-**   * Redistributions of source code must retain the above copyright
-**     notice, this list of conditions and the following disclaimer.
-**   * Redistributions in binary form must reproduce the above copyright
-**     notice, this list of conditions and the following disclaimer in
-**     the documentation and/or other materials provided with the
-**     distribution.
-**   * Neither the name of The Qt Company Ltd nor the names of its
-**     contributors may be used to endorse or promote products derived
-**     from this software without specific prior written permission.
-**
-**
-** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-** OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
-**
-** $QT_END_LICENSE$
-**
-****************************************************************************/
-
 #include <QtWidgets>
 #include <QtWebEngineWidgets>
 
@@ -112,50 +62,7 @@ void MainWindow::finishLoading(bool)
 {
     progress = 100;
     view->page()->runJavaScript(jQuery);
-
-//    rotateImages(rotateAction->isChecked());
 }
-/*
-void MainWindow::highlightAllLinks()
-{
-    QString code = "qt.jQuery('a').each( function () { qt.jQuery(this).css('background-color', 'yellow') } ); undefined";
-    view->page()->runJavaScript(code);
-}
-
-void MainWindow::rotateImages(bool invert)
-{
-    QString code;
-
-    if (invert)
-        code = "qt.jQuery('img').each( function () { qt.jQuery(this).css('-webkit-transition', '-webkit-transform 2s'); qt.jQuery(this).css('-webkit-transform', 'rotate(180deg)') } ); undefined";
-    else
-        code = "qt.jQuery('img').each( function () { qt.jQuery(this).css('-webkit-transition', '-webkit-transform 2s'); qt.jQuery(this).css('-webkit-transform', 'rotate(0deg)') } ); undefined";
-    view->page()->runJavaScript(code);
-}
-
-void MainWindow::removeGifImages()
-{
-    QString code = "qt.jQuery('[src*=gif]').remove()";
-    view->page()->runJavaScript(code);
-}
-
-void MainWindow::removeInlineFrames()
-{
-    QString code = "qt.jQuery('iframe').remove()";
-    view->page()->runJavaScript(code);
-}
-
-void MainWindow::removeObjectElements()
-{
-    QString code = "qt.jQuery('object').remove()";
-    view->page()->runJavaScript(code);
-}
-
-void MainWindow::removeEmbeddedElements()
-{
-    QString code = "qt.jQuery('embed').remove()";
-    view->page()->runJavaScript(code);
-}*/
 
 void KeySpacePress(){
     BYTE keyState[256];
@@ -211,17 +118,7 @@ MainWindow::MainWindow(const QUrl& url):
     QAction* viewSourceAction = new QAction("Page Source", this);
     connect(viewSourceAction, SIGNAL(triggered()), SLOT(viewSource()));
     viewMenu->addAction(viewSourceAction);
-/*
-    QMenu *effectMenu = menuBar()->addMenu(tr("&Effect"));
-    effectMenu->addAction("Highlight all links", this, SLOT(highlightAllLinks()));
 
-    rotateAction = new QAction(this);
-    rotateAction->setIcon(style()->standardIcon(QStyle::SP_FileDialogDetailedView));
-    rotateAction->setCheckable(true);
-    rotateAction->setText(tr("Turn images upside down"));
-    connect(rotateAction, SIGNAL(toggled(bool)), this, SLOT(rotateImages(bool)));
-    effectMenu->addAction(rotateAction);
-*/
     QMenu *toolsMenu = menuBar()->addMenu(tr("&Tools"));
     toolsMenu->addAction(tr("Start Bot"), this, SLOT(startBot()));
     toolsMenu->addAction(tr("End Bot"), this, SLOT(endBot()));
